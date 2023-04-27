b31e5e10596ee8d3f5c9f86bc5fb74e75ca6b0ea
unknown <12617474@alu.murciaeduca.es>
2023-04-27 16:32:49 +0200

    c2: ahora diferencia campos de datos tipo int o String añadiendo el registro correcto

--- src/basesplantilla/BasesPlantilla.java
+++ src/basesplantilla/BasesPlantilla.java
@@ -29,38 +29,70 @@ public class BasesPlantilla {
 
         System.out.println("Inserte datos en orden: ");
 
-        for (int k = 1; k < filas; filas++) {
+        try {
+            for (int k = 1; k <= filas; k++) {
 
-            datos = new String[columnas];
+                datos = new String[columnas];
 
-            for (int i = 0; i < datos.length; i++) {
+                for (int i = 0; i < datos.length; i++) {
 
-                datos[i] = sc.nextLine();
+                    datos[i] = sc.nextLine();
+
+                }
+
+                System.out.println(tablasYdatos(nombreTabla, datos));
 
             }
+        } catch (NullPointerException e1) {
 
-            System.out.println(tablasYdatos(nombreTabla, datos));
+            System.out.println("ERROR. Ingrese datos correctos");
+
+        } catch (IllegalArgumentException e2) {
+
+            System.out.println("ERROR. La tabla debe tener almenos una columna");
 
         }
 
     }
 
-    public static String tablasYdatos(String nombreTabla, String[] datos) {
+    public static String tablasYdatos(String nombreTabla, String[] datos) throws NullPointerException {
 
-        String inicio = "INSERT INTO " + nombreTabla + " VALUES ('";
-        String separador = "','";
+        String inicio = "INSERT INTO " + nombreTabla + " VALUES (";
+        String separador = "',";
+        String separador2 = ",";
         String fin = ");";
+        int caracter = 0;
 
-        for (int i = 0; i < datos.length; i++) {
+        if (datos.length == 0) {
 
-            inicio = inicio + datos[i] + separador;
+            throw new IllegalArgumentException();
+
+        } else {
+
+            for (int i = 0; i < datos.length; i++) {
+
+                while ((caracter < datos[i].length()) && (Character.isDigit(datos[i].charAt(caracter)))) {
+                    caracter++;
+                }
+                if (caracter < datos[i].length()) {
+
+                    inicio = inicio + "'" + datos[i] + separador;
+
+                } else {
+
+                    inicio = inicio + datos[i] + separador2;
+
+                }
+
+                caracter = 0;
+
+            }
+
+            inicio = inicio.substring(0, inicio.length() - 1);
+
+            return inicio + fin;
 
         }
-
-        inicio = inicio.substring(0, inicio.length() - 2);
-
-        return inicio + fin;
-
     }
 
 }
